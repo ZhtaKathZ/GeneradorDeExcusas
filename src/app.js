ece81369c6474686ae35d6cfe7bef2ef10d9a41d
ZhtaKathZ <162374346+ZhtaKathZ@users.noreply.github.com>
2024-03-26 03:53:03 +0000

    KathZ

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuseElement = document.getElementsById("excuse");
+  excuseElement.innerHTML = excuse;
+
+  return (excuse =
+    who[Math.floor(Math.random() * 4)] +
+    action[Math.floor(Math.random() * 4)] +
+    what[Math.floor(Math.random() * 3)] +
+    when[Math.floor(Math.random() * 4)]);
 };
